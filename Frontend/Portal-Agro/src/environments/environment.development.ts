e97c47b336f63fd5cc5380d801987e12fc07bceb
Sergio Andres Leguizamo Vargas <vargasleguizamo95@gmail.com>
2025-07-28 22:21:49 -0500

    Limpiar archivos temporales y aplicar .gitignore correctamente

--- Frontend/Portal-Agro/src/environments/environment.development.ts
+++
@@ -1,3 +0,0 @@
-export const environment = {
-    apiUrl: 'https://localhost:7050/api/v1/'
-};
--- Portal-Agro-comercial-del-Huila/Data/Interfaces/Implements/Producers/Products/IProductImageRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿using Data.Interfaces.IRepository;
-using Entity.Domain.Models.Implements.Products;
-
-namespace Data.Interfaces.Implements.Producers.Products
-{
-    public interface IProductImageRepository : IDataGeneric<ProductImage>
-    {
-    }
-}
