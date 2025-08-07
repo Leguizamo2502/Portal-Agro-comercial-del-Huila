0c31555d267f9c7c7bf9dc2f94562e98d5e8b412
DanielBata <batahernandez12345@gmail.com>
2025-08-06 22:45:50 -0500

    cambiosPrueba

--- Portal-Agro-comercial-del-Huila/Entity/DTOs/Location/Select/CitySelectDto.cs
+++ Portal-Agro-comercial-del-Huila/Entity/DTOs/Location/Select/CitySelectDto.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Entity.DTOs.BaseDTO;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,9 +7,12 @@ using System.Threading.Tasks;
 
 namespace Entity.DTOs.Location.Select
 {
-    public class CitySelectDto
+    public class CitySelectDto : BaseDto
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public string CityName { get; set; }
+        public int CityId { get; set; }
+
     }
 }
