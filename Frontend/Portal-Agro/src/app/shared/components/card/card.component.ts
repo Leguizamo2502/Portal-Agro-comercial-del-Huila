b58a921f64f5b56680850b2b1e06977162600a66
DanielBata <batahernandez12345@gmail.com>
2025-08-10 21:03:58 -0500

    Mejorado-Listar-Productos

--- Frontend/Portal-Agro/src/app/shared/components/card/card.component.ts
+++ Frontend/Portal-Agro/src/app/shared/components/card/card.component.ts
@@ -1,12 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ProductService } from '../../services/product/product.service';
 import { ProductSelectModel } from '../../models/product/product.model';
-import {
-  MatCardModule,
-  MatCardContent,
-
-} from '@angular/material/card';
+import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 
@@ -16,22 +11,12 @@ import { MatIconModule } from '@angular/material/icon';
   imports: [
     CommonModule,
     MatCardModule,
-    MatCardContent,
     MatButtonModule,
     MatIconModule
   ],
   templateUrl: './card.component.html',
   styleUrls: ['./card.component.css']
 })
-export class CardComponent implements OnInit {
-  products: ProductSelectModel[] = [];
-
-  constructor(private productService: ProductService) {}
-
-  ngOnInit(): void {
-    this.productService.getProduct().subscribe({
-      next: (data) => this.products = data,
-      error: (err) => console.error('Error al obtener productos', err)
-    });
-  }
+export class CardComponent {
+  @Input() products: ProductSelectModel[] = [];
 }
