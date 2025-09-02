17de85b41cc5ea4db5b26de73aa14e595fd9339d
Sergio Andres Leguizamo Vargas <vargasleguizamo95@gmail.com>
2025-09-02 07:11:42 -0500

    conectar profile

--- Frontend/Portal-Agro/src/app/features/products/products.routes.ts
+++ Frontend/Portal-Agro/src/app/features/products/products.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from "@angular/router";
 import { ProductDetailComponent } from "../../shared/components/product-detail/product-detail.component";
 import { ProductComponent } from "./pages/product/product.component";
+import { ProducerProfileComponent } from "../producer-profile/producer-profile.component";
 
 export const PRODUCTS_ROUTES: Routes=[
     {path:'',component:ProductComponent},
     {path:':id', component: ProductDetailComponent},
+    {path: 'profile/:code', component: ProducerProfileComponent}
 ];
